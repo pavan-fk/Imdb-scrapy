a90572ad1c061c4181148896a0f0c3d329ca9ea3
pavan-fk <pavank@flipkart.com>
2016-06-19 16:48:51 +0530

    dynamic graph test 1

--- api/DataResource.py
+++ api/DataResource.py
@@ -1,16 +1,15 @@
-from flask import Flask
+from flask import Flask, json
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api=Api(app)
+api = Api(app)
 
-
-class DataResource(Resource):
-    def get():
-        return "Hello world"
-
-
-api.add_resource(DataResource,"/")
+@app.route('/api', methods=['GET'])
+def login():
+    response = json.jsonify({"x": 10, "y": 21, "label": "episode1"}, {"x": 20, "y": 25}, {
+                            "x": 30, "y": 20}, {"x": 40, "y": 25}, {"x": 50, "y": 27})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 # todo: remove debug mode
 if __name__ == '__main__':
